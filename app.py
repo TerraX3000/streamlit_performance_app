641fe9c50e2b952fd4d0b34de34c35221198c4c9
Ken Kranz <ken.kranz57@gmail.com>
2024-10-20 11:20:40 -0400

    add elasped time

--- app.py
+++ app.py
@@ -17,10 +17,9 @@ def run():
                 progress_text = "Operation in progress. Please wait."
                 my_bar = st.progress(0, text=progress_text)
                 for i in range(loops):
-                    percent_complete = int((i + 1) / loops * 100)
                     time.sleep(sleep_seconds)
+                    percent_complete = int((i + 1) / loops * 100)
                     my_bar.progress(percent_complete, text=progress_text)
-                time.sleep(1)
                 my_bar.empty()
                 placeholder.write("Long Running Process Complete!")
 
@@ -29,12 +28,18 @@ def run():
                 loops = st.number_input("Loops", value=10)
                 sleep_seconds = st.number_input("Sleep Seconds Per Loop", value=1)
                 if st.button("Start Longing Running Process"):
+                    start_time = time.time()
                     long_running_process(loops, sleep_seconds)
+                    end_time = time.time()
+                    elapsed_time = end_time - start_time
+                    st.write(f"Elapsed time: {elapsed_time:.2f} seconds")
+                    st.button("Reset Run")
             with col_2:
                 api_delay = st.number_input(
                     "API Delay (seconds)", value=0, min_value=0, max_value=5
                 )
                 if st.button("Execute API Call"):
+                    start_time = time.time()
                     response = requests.get(
                         f"https://dummyjson.com/products?delay={api_delay*1000}"
                     )
@@ -43,6 +48,9 @@ def run():
                         if "products" in data:
                             st.write("API Call Successful!")
                             products = data["products"]
+                            end_time = time.time()
+                            elapsed_time = end_time - start_time
+                            st.write(f"Elapsed time: {elapsed_time:.2f} seconds")
                             st.json(products, expanded=False)
                             st.button("Clear API Response")
 
